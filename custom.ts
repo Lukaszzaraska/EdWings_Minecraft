bb4149a22d5dd0a639d2a9c5c42782fc739ba6e2
Łukasz <79981915+Lukaszzaraska@users.noreply.github.com>
2023-02-20 20:36:52 +0100

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,38 +1,38 @@
-
-// const wheatT = Item.Wheat
-// enum SeedType{
-// //% block = "Wheat"
-// WHEAT
-// }
-enum SeedType {
+const wheatT = Item.Wheat
+enum SeedType{
+    //% blockIdentity="blocks.item"
     //% block="Wheat"
-    Wheat,
-    //% block="Bert"
-    Bert
+    wheatT
 }
+// enum SeedType {
+//     //% block="Wheat"
+//     Wheat,
+//     //% block="Bert"
+//     Bert
+// }
 enum Stage {
-    //% block = "0"
+    //% block="0"
     ZERO=0,
-    //% block = "1"
+    //% block="1"
     ONE,
-    //% block = "2"
+    //% block="2"
     TWO,
-    //% block = "3"
+    //% block="3"
     THREE,
-    //% block = "4"
+    //% block="4"
     FOUR,
-    //% block = "5"
+    //% block="5"
     FIVE,
-    //% block = "6"
+    //% block="6"
     SIX,
-    //% block = "7"
+    //% block="7"
     SEVEN
 }
 //% color=190 weight=100 
 namespace Harvest{
 
-    //% block = "Test  %pos=minecraftCreatePosition"
-    export function SeedStageTest(typeSeed:SeedType,stage:Stage,pos:Position): boolean {
+    //% block="Test $typeSeed stage $stage on %pos=minecraftCreatePosition"
+    export function SeedTest(typeSeed:SeedType,stage:Stage,pos:Position): boolean {
         if (blocks.testForBlock(blocks.blockWithData(typeSeed, stage), pos)) {
             return true
         }else{
