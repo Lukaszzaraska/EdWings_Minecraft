028a19cd9f0769f62f9ed980c5dc9eaf79ea6bdb
Łukasz <79981915+Lukaszzaraska@users.noreply.github.com>
2023-02-21 11:26:03 +0100

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -2,15 +2,15 @@ const enum SeedType {
     //% block="wheat"
     WHEAT="WHEAT",
     //% block="beetroot"
-    BEETROOT ="BEETROOT",
+    BEETROOT="BEETROOT",
     //% block="carrot"
-    CARROT ="CARROT",
+    CARROT="CARROT",
     //% block="potato"
-    POTATO ="POTATO",
+    POTATO="POTATO",
     //% block="pumpkin"
-    PUMPKIN_STEM ="PUMPKIN_STEM",
+    PUMPKIN_STEM="PUMPKIN_STEM",
     //% block="melon"
-    MELON_STEM ="MELON_STEM"
+    MELON_STEM="MELON_STEM"
 }
 enum Stage {
     //% block="0"
